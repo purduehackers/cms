9371ba01d29c2f5d7250227fed76083ad4b34c2a
Jack Hogan <jackhogan11@gmail.com>
2026-01-20 14:44:14 -0500

    oops i forgot to tell the cms it was there

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -9,6 +9,7 @@ import { r2Storage } from '@payloadcms/storage-r2'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { ShelterProjects } from './collections/ShelterProjects'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -28,7 +29,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
+  collections: [Users, Media, ShelterProjects],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
