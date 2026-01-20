4f08b1802aa88305557bfae3b4f87e2048ad4875
Jack Hogan <jackhogan11@gmail.com>
2026-01-20 14:39:29 -0500

    shelter projects + nix

---
+++ src/collections/ShelterProjects.ts
@@ -0,0 +1,36 @@
+import type { CollectionConfig } from 'payload'
+
+export const ShelterProjects: CollectionConfig = {
+  slug: 'shelter-projects',
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'last_division',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'last_owner',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+      required: true,
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+  ],
+}
